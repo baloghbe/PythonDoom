85ab75f53be6a0c74f2628b6821bcf370fabaead
baloghbe <92228708+baloghbe@users.noreply.github.com>
2023-11-08 14:55:51 +0100

    Added npc logic+textures+pathfinding

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from sprite_object import *
 from object_handler import *
 from weapon import *
 from sound import *
+from pathfinding import *
 
 class Game:
     def __init__(self):
@@ -17,6 +18,9 @@ class Game:
         self.screen = pygame.display.set_mode(RES)
         self.clock = pygame.time.Clock()
         self.delta_time = 1
+        self.global_trigger = False
+        self.global_event = pygame.USEREVENT + 0
+        pygame.time.set_timer(self.global_event, 40)
         self.new_game()
 
     def new_game(self):
@@ -27,6 +31,7 @@ class Game:
         self.object_handler = ObjectHandler(self)
         self.weapon = Weapon(self)
         self.sound = Sound(self)
+        self.pathfinding = Pathfinding(self)
 
 
     def update(self):
@@ -46,10 +51,13 @@ class Game:
         #self.player.draw()
 
     def check_events(self):
+        self.global_trigger = False
         for event in pygame.event.get():
             if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
                 pygame.quit()
                 sys.exit()
+            elif event.type == self.global_event:
+                self.global_trigger = True
             self.player.single_fire_event(event)
 
     def run(self):
--- sound.py
+++ sound.py
@@ -6,3 +6,7 @@ class Sound:
         pygame.mixer.init()
         self.path = 'resources/sound/'
         self.shotgun = pygame.mixer.Sound(self.path + 'shotgun.wav')
+        self.npc_pain = pygame.mixer.Sound(self.path + 'npc_pain.wav')
+        self.npc_death = pygame.mixer.Sound(self.path + 'npc_death.wav')
+        self.npc_shot = pygame.mixer.Sound(self.path + 'npc_attack.wav')
+        self.npc_shot.set_volume(0.2)
